90f1d0ccdfa469bd6aa7fa21080451b2e28535d0
Denys Serdiuk <denisserduik@gmail.com>
2024-08-01 21:28:31 +0300

    commit kata from 01.08.2024

--- src/com/denys/studies/Softserve.java
+++ src/com/denys/studies/Softserve.java
@@ -1,10 +1,20 @@
 package com.denys.studies;
 
+import java.util.ArrayList;
+
 public class Softserve {
 
 	public static void main(String[] args) {
 		
+		System.out.println(howMuchILoveYou(50));
 
 	}
-
-}
+	
+	public static String howMuchILoveYou(int nb_petals) {
+	    
+	    String[] petals = {"I love you", "a little", "a lot", "passionately", "madly", "not at all"};
+	    
+	    return petals[(nb_petals-1) % 6];
+	  
+	}
+}	    
